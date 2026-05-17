fd845efd1d9b762b699bdc24b26816214fcccd3c
Tanzim Sheikh <tanzimsheikh68@gmail.com>
2026-05-17 18:01:33 +0530

    upgrating index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   process.env.CLIENT_ORIGIN,
-  "https://nexachatapp.netlify.app",
+  "https://instachatapp.netlify.app",
 ].filter(Boolean);
 
 app.use(
